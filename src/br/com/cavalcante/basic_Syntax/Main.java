675e9b80de975e2da452345f4665d9011644ae04
Mateuzao <mateuzao88@gmail.com>
2021-08-26 19:27:35 -0300

    first commit off java syntax

---
+++ src/br/com/cavalcante/basic_Syntax/Main.java
@@ -0,0 +1,11 @@
+package br.com.cavalcante.basic_Syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("This is a simple example of Java Syntax");
+
+    }
+}
