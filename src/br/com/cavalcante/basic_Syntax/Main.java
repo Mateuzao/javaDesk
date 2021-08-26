d75a61d8e3e1cdda62953a7a945150b5dd637487
Mateuzao <mateuzao88@gmail.com>
2021-08-26 20:05:35 -0300

    java variables

--- src/br/com/cavalcante/basic_Syntax/Main.java
+++ src/br/com/cavalcante/basic_Syntax/Main.java
@@ -4,8 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /**----------------  Java Syntax   ------------------------  */
+        System.out.println("------------------------ Java Syntax ------------------------");
         System.out.println("Hello World");
         System.out.println("This is a simple example of Java Syntax");
+        System.out.println("------------------------------------------------------------------------");
 
+        /**----------------  Java Variables  ------------------------  */
+        System.out.println("------------------------ Java Variables ------------------------");
+        System.out.println("This is a simple Variables");
+        System.out.println("Variables are containers for storing data values.");
+        String name = "Mateus";
+        int myNum;
+                    myNum = 7;
+        String lastname = "Cavalcante";
+        System.out.println("Example:");
+        System.out.println("Name : " +name+" "+lastname);
+        System.out.println("number : "+ myNum);
+        System.out.println("------------------------------------------------------------------------");
     }
 }
